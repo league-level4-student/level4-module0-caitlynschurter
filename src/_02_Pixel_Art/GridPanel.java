d4b9f120a1144d131bd44e3b8f29d852d869b3e4
caitl <caitl@KittyBook2>
2021-01-04 13:32:30 -0800

    Finished Pixel_Art, Started Conways_Game_of_Life   1/4/20

--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -34,12 +34,12 @@ public class GridPanel extends JPanel{
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
 		
 		//2. Initialize the pixel array using the rows and cols variables.
-		pixels = new Pixel[rows][cols];
+		pixels = new Pixel[cols][rows];
 		
 		//3. Iterate through the array and initialize each element to a new pixel.
-		for(int i = 0; i < pixels.length; i++) {
-			for(int j = 0; j < pixels.length; j++) {
-				pixels[i][j] = new Pixel(i, j);
+		for(int x = 0; x < pixels.length; x++) {
+			for(int y = 0; y < pixels.length; y++) {
+				pixels[x][y] = new Pixel(x, y); 
 			}
 		}
 		
@@ -52,6 +52,8 @@ public class GridPanel extends JPanel{
 	public void clickPixel(int mouseX, int mouseY) {
 		//5. Use the mouseX and mouseY variables to change the color
 		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		pixels[mouseX/pixelWidth][mouseY/pixelHeight].color = color;
+		
 	}
 	
 	@Override
@@ -59,10 +61,12 @@ public class GridPanel extends JPanel{
 		//4. Iterate through the array.
 		//   For every pixel in the list, fill in a rectangle using the pixel's color.
 		//   Then, use drawRect to add a grid pattern to your display.
-		
-		for(int i = 0; i < pixels.length; i++) {
-			for(int j = 0; j < pixels.length; j++) {
-				
+		for(int x = 0; x < pixels.length; x++) {
+			for(int y = 0; y < pixels.length; y++) {
+				g.setColor(pixels[x][y].color);
+				g.fillRect(x*pixelWidth, y*pixelHeight, pixelWidth, pixelHeight);
+				g.setColor(Color.BLACK);
+				g.drawRect(x*pixelWidth, y*pixelHeight, pixelWidth, pixelHeight);
 				
 			}
 		}
--- src/_03_Conways_Game_of_Life/WorldPanel.java
+++ src/_03_Conways_Game_of_Life/WorldPanel.java
@@ -19,7 +19,7 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 	private Timer timer;
 	
 	//1. Create a 2D array of Cells. Do not initialize it.
-
+	Cell[][] cells;
 	
 	
 	public WorldPanel(int w, int h, int cpr) {
@@ -29,8 +29,10 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 		this.cellsPerRow = cpr;
 	
 		//2. Calculate the cell size.
+		int cellSize = w/cellsPerRow;
 		
 		//3. Initialize the cell array to the appropriate size.
+		cells
 		
 		//3. Iterate through the array and initialize each cell.
 		//   Don't forget to consider the cell's dimensions when 
